ee796115c0510152a3f57c6fafdd1b52fa2c1bee
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-06-30 10:57:13 +0500

    first commit

---
+++ src/components/NewOpinion.jsx
@@ -0,0 +1,59 @@
+import { useActionState } from "react";
+
+export function NewOpinion() {
+
+  function shareOpinionAction(prevState, formData) {
+    const title = formData.get('title');
+    const body = formData.get('body');
+    const userName = formData.get('userName');
+
+    let errors= [];
+    if(title.trim().length<5){
+      errors.push("Please enter a title atleast 5 characters long!");
+    }if(body.trim().length<10 || body.trim().length<300){
+      errors.push("Please enter description between 10 to 300 characters!");
+    }if(userName.trim().length<0){
+      errors.push("Please enter the username!");
+    }
+    if(errors.length>0){
+      return { errors, enteresValues:{
+        title,
+        body,
+        userName
+      } }
+    }
+  }
+  const [formState, formAction ]=useActionState(shareOpinionAction, {errors: null});
+  return (
+    <div id="new-opinion">
+      <h2>Share your opinion!</h2>
+      <form action={shareOpinionAction}>
+        <div className="control-row">
+          <p className="control">
+            <label htmlFor="userName">Your Name</label>
+            <input type="text" id="userName" name="userName" defaultValue={formState?.enteresValues?.userName} />
+          </p>
+
+          <p className="control">
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" defaultValue={formState?.enteresValues?.title} />
+          </p>
+        </div>
+        <p className="control">
+          <label htmlFor="body">Your Opinion</label>
+          <textarea id="body" name="body" rows={5} defaultValue={formState?.enteresValues?.body} ></textarea>
+        </p>
+        {formState.errors && <ul className="errors">
+          {
+            formState.errors.map((error) =>(
+              <li key={error}>{error}</li>
+            ))
+          }
+          </ul>}
+        <p className="actions">
+          <button type="submit">Submit</button>
+        </p>
+      </form>
+    </div>
+  );
+}
