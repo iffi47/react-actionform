59c5f9d274796371ab4d223c5dad44a97632e3a6
IFTIKHAR AHMAD <66529774+iffi47@users.noreply.github.com>
2026-06-30 12:23:05 +0500

    Add opinion submission functionality and improve form validation
    - Implemented opinion submission in NewOpinion component using context.
    - Enhanced validation for title, body, and username fields.
    - Created a new Submit component for handling form submission state.

--- src/components/NewOpinion.jsx
+++ src/components/NewOpinion.jsx
@@ -1,8 +1,12 @@
-import { useActionState } from "react";
+import { use, useActionState } from "react";
+
+import { OpinionsContext } from "../store/opinions-context";
+import Submit from "./Submit";
 
 export function NewOpinion() {
+  const { addOpinion } = use(OpinionsContext);
 
-  function shareOpinionAction(prevState, formData) {
+  async function shareOpinionAction(prevState, formData) {
     const title = formData.get('title');
     const body = formData.get('body');
     const userName = formData.get('userName');
@@ -10,38 +14,41 @@ export function NewOpinion() {
     let errors= [];
     if(title.trim().length<5){
       errors.push("Please enter a title atleast 5 characters long!");
-    }if(body.trim().length<10 || body.trim().length<300){
+    }if(body.trim().length<10 || body.trim().length>300){
       errors.push("Please enter description between 10 to 300 characters!");
-    }if(userName.trim().length<0){
+    }if(userName.trim().length===0){
       errors.push("Please enter the username!");
     }
     if(errors.length>0){
-      return { errors, enteresValues:{
+      return { errors, enteredValues:{
         title,
         body,
         userName
       } }
     }
+
+    await addOpinion({ title, body, userName });
+    return { errors: null };
   }
   const [formState, formAction ]=useActionState(shareOpinionAction, {errors: null});
   return (
     <div id="new-opinion">
       <h2>Share your opinion!</h2>
-      <form action={shareOpinionAction}>
+      <form action={formAction}>
         <div className="control-row">
           <p className="control">
             <label htmlFor="userName">Your Name</label>
-            <input type="text" id="userName" name="userName" defaultValue={formState?.enteresValues?.userName} />
+            <input type="text" id="userName" name="userName" defaultValue={formState?.enteredValues?.userName} />
           </p>
 
           <p className="control">
             <label htmlFor="title">Title</label>
-            <input type="text" id="title" name="title" defaultValue={formState?.enteresValues?.title} />
+            <input type="text" id="title" name="title" defaultValue={formState?.enteredValues?.title} />
           </p>
         </div>
         <p className="control">
           <label htmlFor="body">Your Opinion</label>
-          <textarea id="body" name="body" rows={5} defaultValue={formState?.enteresValues?.body} ></textarea>
+          <textarea id="body" name="body" rows={5} defaultValue={formState?.enteredValues?.body} ></textarea>
         </p>
         {formState.errors && <ul className="errors">
           {
@@ -50,9 +57,7 @@ export function NewOpinion() {
             ))
           }
           </ul>}
-        <p className="actions">
-          <button type="submit">Submit</button>
-        </p>
+          <Submit/>
       </form>
     </div>
   );
